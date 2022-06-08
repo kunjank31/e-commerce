f6ef51450447a37a51129f8d93a778469a89849d
unknown <kunjank31@gmail.com>
2022-06-08 17:53:18 +0530

    add icons and tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      shop: {
+        first: "#f5fafd",
+        second: "#fcf1ed",
+        third: "#fbf0f4",
+      },
+    },
+  },
+  plugins: [],
+};
